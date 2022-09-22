4d9c4ba84bc897107df56ce479ecdbf610ac5684
kotsukod <kotsukod@gmail.com>
2022-09-22 18:14:54 +0300

    Задание 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while (index <= num)
+{
+    int sqrt = index * index * index;
+    Console.Write(sqrt + ",");
+    index++;
+}
